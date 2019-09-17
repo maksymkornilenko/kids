56444b9de2390ef4cd2e183140b94255c919b54f
Tatiana Lytvyn <litvin.tatianka@gmail.com>
2019-09-17 17:46:57 +0300

    done

--- views/site/index.php
+++ views/site/index.php
@@ -997,19 +997,6 @@ use app\models\LiqPay;
                     </div>
                 </div>
                 <div class="error-send"></div>
-<!--                <form method="POST" accept-charset="utf-8" action="https://www.liqpay.ua/api/3/checkout">-->
-<!--                    <input type="hidden" name="data"-->
-<!--                           value="eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5IjoiaTU3ODM3MzU3OTgxIiwiYW1vdW50IjoiNSIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiLQnNC+0Lkg0YLQvtCy0LDRgCIsInR5cGUiOiJidXkiLCJsYW5ndWFnZSI6InJ1In0="/>-->
-<!--                    <input type="hidden" name="signature" value="E4DA3B7FBBCE2345D7772B0674A318D5"/>-->
-<!--                    <button style="border: none !important; display:inline-block !important;text-align: center !important;padding: 7px 20px !important;-->
-<!--		color: #fff !important; font-size:16px !important; font-weight: 600 !important; font-family:OpenSans, sans-serif; cursor: pointer !important; border-radius: 2px !important;-->
-<!--		background: rgb(122,183,43) !important;" onmouseover="this.style.opacity='0.5';"-->
-<!--                            onmouseout="this.style.opacity='1';">-->
-<!--                        <img src="https://static.liqpay.ua/buttons/logo-small.png" name="btn_text"-->
-<!--                             style="margin-right: 7px !important; vertical-align: middle !important;"/>-->
-<!--                        <span style="vertical-align:middle; !important">Оплатить --><?php //echo $session['cart.sum']?><!-- UAH</span>-->
-<!--                    </button>-->
-<!--                </form>-->
                 <a href="/site/view" class="btn btn-success sendOrder">Оформить заказ</a>
                 <button type="button" class="btn btn-danger clearCart">Очистить корзину</button>
             </div>
